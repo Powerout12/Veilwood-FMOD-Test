7ed8d3547684d59c913825dc8f9bc81563fe7eda
Abner-Wilhelm <abnerwilhelm@gmail.com>
2024-09-19 19:00:13 -0400

    Inventory Item System

---
+++ Assets/Scripts/Item Scripts/InventoryItemData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Inventory System/Inventory Item")]
+public class InventoryItemData : ScriptableObject
+{
+    public int ID;
+    public string displayName;
+    [TextArea(4,4)]
+    public string description;
+    public Sprite icon;
+    public int maxStackSize;
+    public bool canEat;
+    public bool usedForCrafting;
+}
