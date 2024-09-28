6c6d4e2ee2752c7faa354f129237acb0e9c71c16
Cameron Rutkowski <moatsofgoats@gmail.com>
2024-09-28 19:19:50 -0400

    ITEMS WORK WEEEE

--- Assets/Scripts/Item Scripts/PlaceableItem.cs
+++ Assets/Scripts/Item Scripts/PlaceableItem.cs
@@ -6,5 +6,24 @@ using UnityEngine;
 public class PlaceableItem : InventoryItemData
 {
     public GameObject placedPrefab;
+    public bool removeAfterUse = true;
+
+    public void PlaceStructure(Transform player)
+    {
+        Vector3 fwd = player.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+        StructureManager structManager = FindObjectOfType<StructureManager>();
+
+        if(Physics.Raycast(player.position, fwd, out hit, 10, 1 << 7))
+        {
+            Vector3 pos = structManager.CheckTile(hit.point);
+            if(pos != new Vector3(0,0,0)) 
+            {
+                structManager.SpawnStructure(placedPrefab, pos);
+                if(removeAfterUse) HotbarDisplay.currentSlot.AssignedInventorySlot.RemoveFromStack(1);
+            }
+
+        }
+    }
 
 }
