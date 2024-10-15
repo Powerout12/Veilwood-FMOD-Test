d2fcc337f76dce4ef61998704f0a0efe874590a0
McFunny <moatsofgoats@gmail.com>
2024-10-15 14:37:44 -0400

    Shovel Hurts

--- Assets/Scripts/ToolBehaviorScripts/ShovelBehavior.cs
+++ Assets/Scripts/ToolBehaviorScripts/ShovelBehavior.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Tool Behavior", menuName = "Tool Behavior/Shovel")]
 public class ShovelBehavior : ToolBehavior
 {
+    public LayerMask mask;
     public override void PrimaryUse(Transform player, ToolType tool)
     {
         //swing
@@ -12,12 +13,19 @@ public class ShovelBehavior : ToolBehavior
         //HandItemManager.Instance.toolSource.PlayOneShot(swing);
         Vector3 fwd = player.TransformDirection(Vector3.forward);
         RaycastHit hit;
-        if (Physics.Raycast(player.position, fwd, out hit, 4, 1 << 6))
+        if (Physics.Raycast(player.position, fwd, out hit, 4, mask))
         {
+            //DONT MAKE THIS INSTANT, ALIGN IT WITH ANIMATION
             var structure = hit.collider.GetComponent<StructureBehaviorScript>();
             if (structure != null)
             {
                 structure.health -= 2;
+            }
+
+            var creature = hit.collider.GetComponent<CreatureBehaviorScript>();
+            if (creature != null)
+            {
+                creature.TakeDamage(25);
             }
         }
     }
