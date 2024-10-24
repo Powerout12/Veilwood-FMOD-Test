e9b526741b1b0a292360844884e812c685a58b3d
Cameron Rutkowski <moatsofgoats@gmail.com>
2024-10-24 15:07:23 -0400

    Thursday build Stuff

--- Assets/Scripts/Structures/UntilledTile.cs
+++ Assets/Scripts/Structures/UntilledTile.cs
@@ -46,6 +46,7 @@ public class UntilledTile : StructureBehaviorScript
             ignoreNextHour = false;
             return;
         }
+        clearTileOnDestroy = true;
         Destroy(this.gameObject);
     }
 }
